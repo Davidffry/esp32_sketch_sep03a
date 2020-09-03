f03d6b7b99a8cbf04d7d21dd5a9f01f5c08d7e39
Davidffry <david.auffray@axians.com>
2020-09-04 00:28:25 +0200

    function refers to the brooklyn99 characters

--- sketch_sep03a.ino
+++ sketch_sep03a.ino
@@ -1,65 +1,89 @@
 #include <Adafruit_NeoPixel.h>
 
 #define PIN            4
-
-// How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      12
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int delayval = 75; // delay for half a second
+int delayval = 250; 
 int red = 255;
 int green = 50;
 int blue = 0;
+int bright = 255;
+char choix = 't';
 
 void setup() 
 {
-  pixels.begin(); // This initializes the NeoPixel library.
+  pixels.begin(); 
 }
 
 void loop() 
 {
-  left();
+  pixels.setBrightness(bright);
   pixels.clear();
-  right();
-//  white();
-  pixels.clear();
+  rosa();
 }
-void white()
+void bolt()
 {
-    pixels.setPixelColor(0, pixels.Color(255,255,255)); // Moderately bright green color.
-    pixels.setPixelColor(1, pixels.Color(255,255,255)); // Moderately bright green color.
-    pixels.setPixelColor(2, pixels.Color(255,255,255)); // Moderately bright green color.
-    pixels.setPixelColor(3, pixels.Color(255,255,255)); // Moderately bright green color.
-    pixels.setPixelColor(4, pixels.Color(255,255,255)); // Moderately bright green color.
-    pixels.setPixelColor(5, pixels.Color(255,255,255)); // Moderately bright green color.
-    pixels.setPixelColor(6, pixels.Color(255,255,255)); // Moderately bright green color.
-    pixels.setPixelColor(7, pixels.Color(255,255,255)); // Moderately bright green color.
-    pixels.setPixelColor(8, pixels.Color(255,255,255)); // Moderately bright green color.
-    pixels.setPixelColor(9, pixels.Color(255,255,255)); // Moderately bright green color.
-    pixels.setPixelColor(10, pixels.Color(255,255,255)); // Moderately bright green color.
-    pixels.setPixelColor(11, pixels.Color(255,255,255)); // Moderately bright green color.
-    pixels.setPixelColor(12, pixels.Color(255,255,255)); // Moderately bright green color.
-    pixels.show(); // This sends the updated pixel color to the hardware.
-    delay(delayval); // Delay for a period of time (in milliseconds).
+    pixels.setPixelColor(0, pixels.Color(255,255,255)); 
+    pixels.setPixelColor(1, pixels.Color(255,255,255)); 
+    pixels.setPixelColor(2, pixels.Color(255,255,255)); 
+    pixels.setPixelColor(3, pixels.Color(255,255,255)); 
+    pixels.setPixelColor(4, pixels.Color(255,255,255)); 
+    pixels.setPixelColor(5, pixels.Color(255,255,255)); 
+    pixels.setPixelColor(6, pixels.Color(255,255,255)); 
+    pixels.setPixelColor(7, pixels.Color(255,255,255)); 
+    pixels.setPixelColor(8, pixels.Color(255,255,255)); 
+    pixels.setPixelColor(9, pixels.Color(255,255,255)); 
+    pixels.setPixelColor(10, pixels.Color(255,255,255)); 
+    pixels.setPixelColor(11, pixels.Color(255,255,255)); 
+    pixels.setPixelColor(12, pixels.Color(255,255,255)); 
+    pixels.show(); 
 }
-void left()
+void amy()
 {
+  while(true){
     for(int i=0;i<NUMPIXELS;i++)
   {
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(red,green,blue)); // Moderately bright green color.
-    pixels.show(); // This sends the updated pixel color to the hardware.
-    delay(delayval); // Delay for a period of time (in milliseconds).
+    
+    pixels.setPixelColor(i, pixels.Color(red,green,blue)); 
+    pixels.show(); 
+    delay(delayval); 
+  }
+  pixels.clear();
   }
 }
-void right()
+void jake()
 {
     for(int i=NUMPIXELS;i>=0;i--)
   {
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(red,green,blue)); // Moderately bright green color.
-    pixels.show(); // This sends the updated pixel color to the hardware.
-    delay(delayval); // Delay for a period of time (in milliseconds).
+    
+    pixels.setPixelColor(i, pixels.Color(red,green,blue)); 
+    pixels.show(); 
+    delay(delayval); 
+  }
+}
+void rosa()
+{
+  switch (choix)
+  {
+  case 'l':
+    pixels.clear();
+    amy();
+    break;
+  case 'r':
+    pixels.clear();
+    jake();
+    break;
+  case 's':
+    pixels.clear();
+    break;
+  case 't':
+    pixels.clear();
+    bolt();
+    break;
+  default:
+    pixels.clear();
+    break;
   }
 }
